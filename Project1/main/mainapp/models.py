05c7f0d5f594a9253af1178f9c58953befddb6a0
ReverendSafari <s.swanson24@ncf.edu>
2024-02-10 19:57:35 -0500

    Finished Reading Material Template/View
    Reading Material page is up and functional, integrating and working with the DB. Just needs styling, as well as extra code user auth if we go down that path. I am doing the base template next, and then moving onto the pomodoro timer.

--- Project1/main/mainapp/models.py
+++ Project1/main/mainapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class readingMaterial(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    link = models.URLField()
+    read = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
---
+++ Project1/main/mainapp/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+        path('reading-list/', views.reading_material_view, name='reading-list'),
+]

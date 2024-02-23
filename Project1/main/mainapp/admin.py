943ff5259ac5a760c6587c98620a855a8ea16bb6
Kinjen0 <118626914+Kinjen0@users.noreply.github.com>
2024-02-22 19:29:11 -0500

    for some reason it didnt catch my changes to admin.py..

--- Project1/main/mainapp/admin.py
+++ Project1/main/mainapp/admin.py
@@ -3,7 +3,7 @@ from .models import Event
 from .models import readingMaterial, classList
 
 
-# Register your models here.
+# Register your models here. Basically written by copilot. I wrote the first 3 letters and it finished it for me
 admin.site.register(Event) 
 admin.site.register(readingMaterial)
 admin.site.register(classList)
